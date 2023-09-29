3674031a17dea91493011b671dd44ea100c21b99
Piotr Grochowiecki <Piotr.Grochowiecki@dxc.com>
2023-09-29 18:48:51 +0200

    Initial commit

---
+++ src/main/java/com/piotrgrochowiecki/carpositioningwebservice/CarPositioningWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.piotrgrochowiecki.carpositioningwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPositioningWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarPositioningWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/piotrgrochowiecki/carpositioningwebservice/CarPositioningWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piotrgrochowiecki.carpositioningwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPositioningWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
