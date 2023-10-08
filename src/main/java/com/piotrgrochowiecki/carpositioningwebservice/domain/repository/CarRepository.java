69cba1bd9679e3cedf6a19baf3cc55f43facb63c
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2023-10-08 18:48:41 +0200

    improvements to NotFoundException (#10)
    refactoring of code

--- src/main/java/com/piotrgrochowiecki/carpositioningwebservice/domain/repository/CarRepository.java
+++ src/main/java/com/piotrgrochowiecki/carpositioningwebservice/domain/repository/CarRepository.java
@@ -6,6 +6,7 @@ import com.piotrgrochowiecki.carpositioningwebservice.domain.exception.NotFoundE
 import com.piotrgrochowiecki.carpositioningwebservice.domain.mapper.CarMapper;
 import com.piotrgrochowiecki.carpositioningwebservice.domain.model.Car;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -23,10 +24,14 @@ public class CarRepository {
         this.carMapper = carMapper;
     }
 
-    public Car findByUuid(String uuid) throws NotFoundException {
+    public Car findByUuid(@Nullable String uuid) throws NotFoundException {
         Optional<CarEntity> optionalCarEntity = carCRUDRepository.findCarByUuid(uuid);
-        CarEntity carEntity = optionalCarEntity.orElseThrow(() -> new NotFoundException(uuid));
+        CarEntity carEntity = optionalCarEntity.orElseThrow(() -> new NotFoundException("Car", uuid));
         return carMapper.mapEntityToModel(carEntity);
+    }
+
+    public boolean existsByUuid(@Nullable String uuid) {
+        return carCRUDRepository.existsByUuid(uuid);
     }
 
     public void save(Car car) {
--- src/main/java/com/piotrgrochowiecki/carpositioningwebservice/domain/service/PositionService.java
+++ src/main/java/com/piotrgrochowiecki/carpositioningwebservice/domain/service/PositionService.java
@@ -1,38 +1,48 @@
 package com.piotrgrochowiecki.carpositioningwebservice.domain.service;
 
+import com.piotrgrochowiecki.carpositioningwebservice.domain.exception.NotFoundException;
 import com.piotrgrochowiecki.carpositioningwebservice.domain.model.Position;
+import com.piotrgrochowiecki.carpositioningwebservice.domain.repository.CarRepository;
 import com.piotrgrochowiecki.carpositioningwebservice.domain.repository.PositionRepository;
 import com.piotrgrochowiecki.carpositioningwebservice.domain.service.supplier.GeoDataSupplier;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Optional;
 
 @Service
 public class PositionService {
 
     private final GeoDataSupplier geoDataSupplier;
-
     private final PositionRepository positionRepository;
+    private final CarRepository carRepository;
 
     @Autowired
-    public PositionService(GeoDataSupplier geoDataSupplier, PositionRepository positionRepository) {
+    public PositionService(GeoDataSupplier geoDataSupplier, PositionRepository positionRepository, CarRepository carRepository) {
         this.geoDataSupplier = geoDataSupplier;
         this.positionRepository = positionRepository;
+        this.carRepository = carRepository;
+    }
+    public Optional<Position> getCurrentPositionOfACarByUuidAndSaveIt(@Nullable String uuid) throws NotFoundException {
+        if (!carRepository.existsByUuid(uuid)){
+            return Optional.empty();
+        }
+        Position position = generatePosition(uuid);
+        positionRepository.save(position);
+        return Optional.of(position);
     }
 
-    public Position getCurrentPositionOfACarByUuidAndSaveIt(String uuid) {
-        Position position = Position.builder()
+    private Position generatePosition(String uuid) {
+        return Position.builder()
                 .latitude(geoDataSupplier.randomLatitudeSupplier.get())
                 .longitude(geoDataSupplier.randomLongitudeSupplier.get())
                 .time(LocalTime.now())
                 .date(LocalDate.now())
                 .carsUuid(uuid)
                 .build();
-
-        positionRepository.save(position);
-        return position;
     }
 
 }
